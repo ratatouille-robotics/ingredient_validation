31c6ef6f7006e84f51fd114baa1f640e04c1e7d1
Sai Shruthi Balaji <shruthibalaji2307@gmail.com>
2022-11-17 23:41:48 -0500

    "Fixed test client"

--- src/validate_ingredient_test_client.py
+++ src/validate_ingredient_test_client.py
@@ -5,13 +5,12 @@ from ingredient_validation.srv import ValidateIngredient
 # Example client node
 def validate_ingredient_client():
     try:
-        while(1):
-            rospy.wait_for_service('ingredient_validation')
-            service_call = rospy.ServiceProxy('ingredient_validation', ValidateIngredient)
-            print("Calling service")
-            response = service_call(mode='rgb', ingredient_name="no_ingredient")
-            print(response)
-            return response
+        rospy.wait_for_service('ingredient_validation')
+        service_call = rospy.ServiceProxy('ingredient_validation', ValidateIngredient)
+        print("Calling service")
+        response = service_call(mode='rgb', ingredient_name="no_ingredient")
+        print(response)
+        return response
     except rospy.ServiceException as e:
         print("Service call failed: %s"%e)
 
